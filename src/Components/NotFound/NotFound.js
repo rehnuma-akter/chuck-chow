8ec569f34f75a392fe2f1620776b5e7a998a1eb4
ruhi <raktterruhi@gmail.com>
2022-06-06 22:33:03 +0600

    404 error page added

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1> 404 error</h1>
+        <div className='error mt-5'>
+            <h3>Unavailable</h3>
+            <img src="https://i.ibb.co/BgrWD21/error.png" alt="" />
         </div>
     );
 };
