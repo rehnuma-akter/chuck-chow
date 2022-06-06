4b7d51cd86bc78ce504d65a76e7dceb96581268c
ruhi <raktterruhi@gmail.com>
2022-06-06 21:03:49 +0600

    worked on the  about me component

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const About = () => {
+const AboutMe = () => {
     return (
-        <div>
-            <h1>about me</h1>
+        <div className='container-fluid my-5 py-5'>
+            <h3 className='py-3'>Hey I am Rehnuma, Nice to Meet you !!!</h3>
+            <p className='mx-4 pb-5 mb-5'> I am currently completing my undergraduate studies in Computer Science.
+                Few months back I was really interested to learn web development and in that process I came across  Programming
+                Hero's web development course which really helped me understand the concepts well with necessary
+                guidance. Now even though I don't have the proper knowledge about everything that goes into the web, yet I feel
+                confident enough to explore more as I go deep into the process.
+                From here my goal is to sharpen my skills even more and try to help as many people as I can
+                with my acquired knowledge. Also looking forward to build exciting stuffs through the opportunities I get in my way.
+                
+            </p>
         </div>
     );
 };
 
-export default About;
+export default AboutMe;
